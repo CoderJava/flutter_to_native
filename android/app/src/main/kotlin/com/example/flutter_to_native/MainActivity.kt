cc59165ca7e6413ab3815c6f5ae75428d2d06a62
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-10-29 21:30:47 +0700

    Create function platform channel in Android and iOS code to call from Flutter code

--- android/app/src/main/kotlin/com/example/flutter_to_native/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_to_native/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.flutter_to_native
 
 import android.os.Bundle
+import android.util.Log
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    val channel = "flutter.native/helper"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        MethodChannel(flutterView, channel).setMethodCallHandler { call, result ->
+            if (call.method == "greetingFromNativeCode") {
+                val greeting = "Greeting from Android Code"
+                result.success(greeting)
+            }
+        }
+    }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,16 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "flutter.native/helper", binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        guard call.method == "greetingFromNativeCode" else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+        result("Greeting from iOS Code")
+    })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
